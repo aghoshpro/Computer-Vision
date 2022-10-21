c59df66d2eeaf7f9f73f15e88c5de0dd28fe1fb7
Arka Ghosh <71174892+aghoshpro@users.noreply.github.com>
2022-10-21 17:23:27 +0200

    Update objectDetectionWebCam.py

--- WASP Object Detection Part 2/objectDetectionWebCam.py
+++ WASP Object Detection Part 2/objectDetectionWebCam.py
@@ -29,7 +29,7 @@ weightsPath = 'frozen_inference_graph.pb'
 # Deep Neural Network (DNN) Model development
 model_01 = cv2.dnn_DetectionModel(weightsPath, configPath) #from openCV, few parameters can be changed from documentation
 
-#creating blob from the frames of the Video
+# Creating blob from the frames of the Video
 model_01.setInputSize(320, 320)  # input size that SSD models generally expect in almost all frameworks. It is the same for TensorFlow as well.
 model_01.setInputScale(1.0/ 127.5) # openCV docs
 model_01.setInputMean((127.5, 127.5, 127.5)) # means for 3 different channels where highest is 255
